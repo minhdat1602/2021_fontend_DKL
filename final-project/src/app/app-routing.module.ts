9e3dd345213d1b35730883f49ede648b2d356648
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2021-06-30 08:18:16 +0700

    Add Observer pattern for cart

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
+  { path: 'cart', component: CartComponent},
   { path: 'products', loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule) }
 ];
 
