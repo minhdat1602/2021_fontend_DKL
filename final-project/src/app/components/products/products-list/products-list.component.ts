2ccde1a86235e628d96f66dc2d79a787b2e39c49
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2021-07-10 13:26:38 +0700

    Add pipe controller

--- final-project/src/app/components/products/products-list/products-list.component.ts
+++ final-project/src/app/components/products/products-list/products-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service'; 
+import { ProductService } from 'src/app/services/product.service';
 import { FormatNumberService } from '../../../services/format-number.service';
 @Component({
   selector: 'app-listproducts',
@@ -16,7 +16,7 @@ export class ProductsListComponent implements OnInit {
   constructor(
     private productService: ProductService,
     public formatNumberService: FormatNumberService
-    ) { }
+  ) { }
 
   products: any[] = [];
   ngOnInit(): void {
@@ -27,13 +27,13 @@ export class ProductsListComponent implements OnInit {
     this.productService.getAllProduct().subscribe(
       item => {
         this.products = item
-      } 
+      }
     )
   }
 
-  tabSize(event: any){
+  tabSize(event: any) {
     this.page = event;
     this.showData();
-  }  
-  
+  }
+
 }
--- final-project/src/app/components/products/products.module.ts
+++ final-project/src/app/components/products/products.module.ts
@@ -6,12 +6,16 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductSingleComponent } from './product-single/product-single.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FilterPipe } from 'src/app/pipes/filter.pipe';
+import { PriceFilterPipe } from '../../pipes/price-filter.pipe';
 
 
 @NgModule({
   declarations: [
     ProductsListComponent,
-    ProductSingleComponent
+    ProductSingleComponent,
+    FilterPipe,
+    PriceFilterPipe
   ],
   imports: [
     CommonModule,
