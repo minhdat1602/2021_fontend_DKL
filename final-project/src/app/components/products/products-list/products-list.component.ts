dd9873cd0ecab10ba7ca26ba02fa260a4f9fb50c
thanhlong3200 <18130131@st.hcmuaf.edu.vn>
2021-07-12 12:00:55 +0700

    add pipe search

--- final-project/src/app/components/products/products-list/products-list.component.ts
+++ final-project/src/app/components/products/products-list/products-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
+import { ProductService } from 'src/app/services/product.service'; 
 import { FormatNumberService } from '../../../services/format-number.service';
 @Component({
   selector: 'app-listproducts',
@@ -13,10 +13,16 @@ export class ProductsListComponent implements OnInit {
   tableSize = 9;
   tableSizesArr = [4, 8, 12];
 
+  searchText: string = '';
+
+  filterPrice: number = 1;
+
+  priceValue: number = 10000000
+
   constructor(
     private productService: ProductService,
     public formatNumberService: FormatNumberService
-  ) { }
+    ) { }
 
   products: any[] = [];
   ngOnInit(): void {
@@ -27,13 +33,18 @@ export class ProductsListComponent implements OnInit {
     this.productService.getAllProduct().subscribe(
       item => {
         this.products = item
-      }
+      } 
     )
   }
 
-  tabSize(event: any) {
+  tabSize(event: any){
     this.page = event;
     this.showData();
+  }  
+  
+  filterPriceFunc() {
+    this.priceValue = 5000000 + this.filterPrice * 100000
+    console.log(this.filterPrice)
   }
 
 }
