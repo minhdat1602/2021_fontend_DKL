65a9aa6fa7c2d554ebd369d260518be666d8230b
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2021-09-01 13:56:00 +0700

    Fix some bug

--- final-project/src/app/components/checkout/checkout.component.ts
+++ final-project/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { FormatNumberService } from '../../services/format-number.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +11,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  localCart: any = [];
+  subTotalPrice: number = 0
+  tax: number = 0;
+  total: number = 0;
+
+  constructor(
+    private cartService: CartService,
+    public formatNumberService: FormatNumberService,
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.loadCart()
+    this.getSubTotalPrice()
+    this.getTax()
+    this.getTotal()
+    this.CartDetails()
+    this.userDetail()
+  }
+
+  loadCart() {
+    if (localStorage.getItem('localCart')) {
+      this.localCart = JSON.parse(localStorage.getItem('localCart') || '{}')
+    }
+  }
+
+
+  getSubTotalPrice() {
+    if (localStorage.getItem('localCart')) {
+      this.localCart = JSON.parse(localStorage.getItem('localCart') || '{}')
+      this.subTotalPrice = this.localCart.reduce((acc: any, val: any) => {
+        return acc + (val['product']['priceOfPiece'] * val['product']['quantity'])
+      }, 0)
+    }
+  }
+
+  getTax() {
+    this.tax = this.subTotalPrice * 12 / 100;
+  }
+
+  getTotal() {
+    this.total = this.subTotalPrice + this.tax
+  }
+
+  navigate() {
+    if (this.authService.isLoggedIn()) {
+      this.cartService.removeAll()
+      this.router.navigate(['/success'])
+      this.loadCart()
+      this.cartNumberFunc()
+    }
+    this.router.navigate(['/login'])
+  }
+
+  cartNumber: number = 0
+  cartNumberFunc() {
+    let count = 0
+    let cartValue = JSON.parse(localStorage.getItem('localCart') || '{}')
+    this.cartNumber = cartValue.length
+    for (let i = 0; i < cartValue.length; i++) {
+      count += cartValue[i]['product']['quantity']
+    }
+    this.cartNumber = count
+    this.cartService.cartSubject.next(this.cartNumber)
+  }
+
+  getCartDetails: any = []
+  CartDetails() {
+    if (localStorage.getItem('localCart')) {
+      this.getCartDetails = JSON.parse(localStorage.getItem('localCart') || '{}')
+    }
+  }
+
+  getUserDetails: any
+  userDetail() {
+    if (localStorage.getItem('user')) {
+      this.getUserDetails = JSON.parse(localStorage.getItem('user') || '{}')
+    }
+    console.log(this.getUserDetails)
   }
 
 }
