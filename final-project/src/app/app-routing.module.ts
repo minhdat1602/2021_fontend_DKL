cd6805e3c0f317537547fd664a953c1539b813c7
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2021-09-08 09:39:17 +0700

    Update introduct screen

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { SuccessComponent } from './components/success/success.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { IntroduceComponent } from './components/introduce/introduce.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'success', component: SuccessComponent },
+  { path: 'introduce', component: IntroduceComponent },
   { path: 'products', loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule) },
   { path: '**', component: PagenotfoundComponent }
 ];
