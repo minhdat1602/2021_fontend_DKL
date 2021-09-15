932b58bf34789d55e3196680aa406902ed4acf95
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2021-09-15 09:04:00 +0700

    Update screen checkout and success after checkout

--- final-project/src/app/components/success/success.component.ts
+++ final-project/src/app/components/success/success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormatNumberService } from '../../services/format-number.service';
 
 @Component({
   selector: 'app-success',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public formatNumberService: FormatNumberService
+  ) { }
 
   ngOnInit(): void {
   }
